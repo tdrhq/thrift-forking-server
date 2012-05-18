556a28e72c13c8210340c89b48198a5d6b1c2eea
Arnold Noronha <arnstein87@gmail.com>
2012-05-18 10:41:01 -0700

    initial commit.

---
+++ lib/thrift-forking-server.rb
@@ -0,0 +1,9 @@
+require "thrift-forking-server/version"
+
+module Thrift
+  module Forking
+    module Server
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/thrift-forking-server/version.rb
@@ -0,0 +1,7 @@
+module Thrift
+  module Forking
+    module Server
+      VERSION = "0.0.1"
+    end
+  end
+end
